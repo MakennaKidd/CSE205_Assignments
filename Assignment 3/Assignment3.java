9afa038c0f20eaa4a9c4eb7ce25d048bf0ab211b
MakennaKidd <72726670+MakennaKidd@users.noreply.github.com>
2020-10-12 13:57:21 -0700

    Add files via upload

---
+++ Assignment 3/Assignment3.java
@@ -0,0 +1,37 @@
+Assignment #: 3
+	Name: Makenna Kidd
+   StudentID: 1218102912
+     Lecture: Tues + Thurs 1:30 - 2:45
+
+
+1. Software engineers shall ensure that their products and related modifications meet the highest professional standards possible.
+
+	- Software engineers should ensure all products and code have gone through substantial amount of debugging, privacy settings, tests, and
+	  construction. If not done so, there could be a privacy breach resulting in many users information leaked and other problem to occur.
+	  If the correct preventions were done, a multitude of issues couldve been prevented without the lack of carelessness.
+
+2. Software engineers shall advance the integrity and reputation of the profession consistent with the public interest.
+
+	- Developers of any kind should always educate themselves on all laws corresponding with coding, follow the ethics of being 
+	  a developer, and avoid companies that do not represent these laws and ethics. Regarding these ethics, a developer should
+	  always take responsibility when aware of an error and then promptly correct it otherwise it could be detrimental to
+	  consumers and themselves. 
+
+3. Software engineers shall be fair to and supportive of their colleagues. 
+
+	- When working with other developers, each person should be supportive of their work, remind them of the laws and ethics of being a
+	  developer, and never steal any of their work. Being a developer that works with others developers should embrace a team
+	  mentality to ensure a healthy work place that promotes ethical and practical work being produced.
+
+4. Software engineers shall participate in lifelong learning regarding the practice of their profession and shall promote an ethical 
+   approach to the practice of the profession.
+
+	- When being a developer, it is important to obtain new information throughout the tech world in order to be updated on new processes,
+	  out of date technologies, new technologies, and other related topics. When a developer strives to be better and stay updated on education,
+	  it ensures that they are aware of what does and doesnt work. Thus resulting in smarter developing that could avoid mistakes.
+
+5. Software engineers shall maintain integrity and independence in their professional judgment.
+
+	-When being a developer, they shall not participate in deceptive behavior that could result in ultimately hurting others. For instance, 
+	 developers should not participate in double billing, tampering with someones accounts or information, or anything that could benefit them
+	 in the trade of hurting others. They should keep professional practices at all time because they deal with delicate information. 
